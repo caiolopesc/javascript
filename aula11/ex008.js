958d94b35fbf595ab6347f4bd87c8e2ca485bc37
Caio <114187415+caiolopesc@users.noreply.github.com>
2023-03-22 17:19:21 -0300

    criação exercícios aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 69
+console.log(`A velocidade do carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Utilize sempre o cinto de segurança.')
