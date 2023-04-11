b62fd62b9ada31e8363b331fd2022168437acdea
Caio <114187415+caiolopesc@users.noreply.github.com>
2023-04-11 16:17:53 -0300

    criação aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+//ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO FINAL
+var c = 1
+do {
+    console.log('Olá, Mundo!')
+    c++ // c++ significa c = c + 1 
+} while (c <= 6)
+
+
+/*
+ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO INÍCIO
+var c = 1
+while (c <= 6) {
+    console.log('Olá, Mundo!')
+    c++ // c++ significa c = c + 1 
+}
+*/
