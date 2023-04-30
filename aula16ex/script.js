51c9239b4b848ec57b6fcc8b4c844df998f01136
Caio <114187415+caiolopesc@users.noreply.github.com>
2023-04-30 15:38:45 -0300

    atualizaçao aula 16

--- aula16ex/script.js
+++ aula16ex/script.js
@@ -22,8 +22,37 @@ function inLista(n, l) {
 
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores)) {
-        
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
+    }
+
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if(valores.lenght == 0) {
+        window.alert('Adicione valores antes de finalizar.')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        for (let pos in valores) {
+            if (valores[pos] > maior) {
+                maior = valores[pos]
+            }
+            if (valores[pos] < menor) {
+                menor = valores[pos]
+            }
+        }
+        res.innerHTML = ''
+        res.innerHTML += <p>`Ao todo temos ${tot} números cadastrados.`</p> 
+        res.innerHTML += `O maior valor informado foi ${maior}.`
+        res.innerHTML += `O menor valor informado foi ${menor}.`
     }
 }
